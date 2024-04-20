c3c4e93088b4fc26f225c98cf21e0b1b6b5e098b
serhiyserhiychuk <141840178+serhiyserhiychuk@users.noreply.github.com>
2024-04-20 09:49:13 -0400

    add middleawares

--- server.js
+++ server.js
@@ -1,10 +1,24 @@
 import express from "express";
 import { PORT } from "./src/config.js";
+import logger from "morgan"
+import cors from "cors";
 const app = express();
 
+app.use(logger("dev"))
+
+app.use(cors());
+
+app.use(express.json());
+
 app.get("/", (req, res) => {
-  res.json("Hello Back-end!");
+  res.json("Hello Back-end!").status(230);
 });
+
+app.use((_, res) => {
+  res.json({
+    message: "Not found"
+  }).status(404)
+})
 
 app.listen(PORT, (err) => {
   if (err) {
